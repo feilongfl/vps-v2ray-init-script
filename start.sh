eb0307ec635b073d4394e130da859819dc9a5ed8
feilong <614183595@qq.com>
2018-02-08 11:27:54 +0800

    add nload

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tmux new "htop" ';' split -h "sh ./v2ray.sh" ';' split -l 20 "sh ./kcptun.sh"';' split -l 10 "sh ./udp2raw.sh"
+tmux new "htop" ';' split -h "sh ./v2ray.sh" ';' split -l 20 "sh ./kcptun.sh"';' split -l 10 "sh ./udp2raw.sh"';' split -t 1 "nload"
